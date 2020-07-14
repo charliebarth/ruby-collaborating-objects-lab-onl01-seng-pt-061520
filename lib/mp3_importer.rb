190412097fe880b71b2f76a4ab8500ed4e7cb6d0
Charlie Barth <charliepbarth@gmail.com>
2020-07-14 01:43:15 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,8 @@ class MP3Importer
   end 
   
   def files
-    #Dir[@path + "/*.mp3"]
-    Dir.entries("#{@path}" + "/*.mp3").select {|f| !File.directory? f}
+    Dir[@path + "/*.mp3"].select {|f| !File.directory? f}
+    #Dir.entries("#{@path}" + "/*.mp3").select {|f| !File.directory? f}
     
   end 
   
